c65025fc6e26859227ee200c42056ea239d5b824
ZackBridges <zacharybridges@MacBook-Air.local>
2015-03-03 09:08:48 -0500

    Modularizing functions and cleaning up code styling
    Pulled the function displayAnswer out of the calculateConvRate
    function. displayAnswer was set up to accept an argument, output, but I
    was just literally passing the variable answer, which was local to
    calculateConvRate. Now all functions are completely modular, and
    operate independently. Code still works, obviously. Also cleaned up
    some styling. Removed unnecessary white space and improved the wording
    of comments.

--- ConvRateCalculator.js
+++ ConvRateCalculator.js
@@ -1,4 +1,4 @@
-// Calculate button event handler
+// 'Calculate' button event handler
 function init () {
     var calculateButton = document.getElementById("calculate");
     calculateButton.onclick = handleCalculateButton;
@@ -7,8 +7,7 @@ function init () {
 // Gets input and calls calculator function
 function handleCalculateButton() {
     var pvInput = document.getElementById("pageviews");
-    var uPageViews = pvInput.value;
-    
+    var uPageViews = pvInput.value; 
     var subInput = document.getElementById("submissions");
     var uSubmissions = subInput.value;
     
@@ -19,15 +18,15 @@ function handleCalculateButton() {
 function calculateConvRate(pageViews, submissions) {
     // Convert the result to percentage then round to the nearest hundredth
     var convRate = Math.round(((submissions/pageViews) * 100) * 100) / 100;
-     var answer = convRate + "%";
-    
-    // Format the answer for HTML
-    function displayAnswer(output) {
-        var answerArea = document.getElementById("answer");
-        answerArea.innerHTML = answer;
-    }
-    
+    var answer = convRate + "%";
+
     displayAnswer(answer)
+}
+
+// Push the answer to HTML element
+function displayAnswer(output) {
+    var answerArea = document.getElementById("answer");
+    answerArea.innerHTML = output;
 }
     
 window.onload = init;
