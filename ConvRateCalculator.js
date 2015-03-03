7bbbfc76f93689aedbd8354845400aae12d7bc68
ZackBridges <zacharybridges@MacBook-Air.local>
2015-03-03 17:51:26 -0500

    Streamlining with jQuery
    I learned jQuery and was able to thin out some chunks of code.
    Consolidated the whole handleCalculateButton function into a tiny
    3-line function in the init function. Slimmed down the displayAnswer
    function too. Might try to work that one into the click handler next.
    Makes sense that the calculate function should be the only function
    separate from the click handler.
    
    Finally, changed the javascript window.onload method to the jQuery
    $(document).ready() method. This will probably approve usability in the
    future if some unfortunate soul uses this code on their website.

--- ConvRateCalculator.js
+++ ConvRateCalculator.js
@@ -1,32 +1,25 @@
+// Works in jQuery
 // 'Calculate' button event handler
 function init () {
-    var calculateButton = document.getElementById("calculate");
-    calculateButton.onclick = handleCalculateButton;
+    $("#calculate").click(function() {
+        calculateConvRate($("#pageviews").val(), $("#submissions").val());
+    });
 }
 
-// Gets input and calls calculator function
-function handleCalculateButton() {
-    var pvInput = document.getElementById("pageviews");
-    var uPageViews = pvInput.value; 
-    var subInput = document.getElementById("submissions");
-    var uSubmissions = subInput.value;
-    
-    calculateConvRate(uPageViews, uSubmissions);
-}
-
+// I'll probably just leave this one alone for now.
 // Calculate and display the conversion rate
 function calculateConvRate(pageViews, submissions) {
     // Convert the result to percentage then round to the nearest hundredth
     var convRate = Math.round(((submissions/pageViews) * 100) * 100) / 100;
     var answer = convRate + "%";
 
-    displayAnswer(answer)
+    displayAnswer(answer);
 }
 
 // Push the answer to HTML element
 function displayAnswer(output) {
-    var answerArea = document.getElementById("answer");
-    answerArea.innerHTML = output;
+    $("#answer").html(output);
 }
     
-window.onload = init;
+// Works in jQuery
+$(document).ready(init);
