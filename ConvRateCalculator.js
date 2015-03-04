b88eab9b7abf525908565e75c1cfe830111c48cb
ZackBridges <zacharybridges@MacBook-Air.local>
2015-03-04 10:44:02 -0500

    More jQuery rewrites
    Got rid of the displayAnswer function and moved it up into a single
    line in the calculateConvRate function

--- ConvRateCalculator.js
+++ ConvRateCalculator.js
@@ -1,4 +1,3 @@
-// Works in jQuery
 // 'Calculate' button event handler
 function init () {
     $("#calculate").click(function() {
@@ -6,20 +5,14 @@ function init () {
     });
 }
 
-// I'll probably just leave this one alone for now.
 // Calculate and display the conversion rate
 function calculateConvRate(pageViews, submissions) {
     // Convert the result to percentage then round to the nearest hundredth
     var convRate = Math.round(((submissions/pageViews) * 100) * 100) / 100;
     var answer = convRate + "%";
 
-    displayAnswer(answer);
+    $("#answer").text(answer);
 }
 
-// Push the answer to HTML element
-function displayAnswer(output) {
-    $("#answer").html(output);
-}
-    
-// Works in jQuery
+// Initialize functions
 $(document).ready(init);
